903ee789ba45221ea9e1ace4d55688fc98fe3c2c
X <33039497+x@users.noreply.github.com>
2019-08-01 21:06:49 +0200

    bug1

--- project1/app/Lara.php
+++ project1/app/Lara.php
@@ -3,13 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Lara extends Model
 {
     //
       public function duration() {
           $temp = $this->time;
-          $duration = $temp->timediffInSeconds(Carbon\Carbon::now());
+        //  $duration = $temp->timediffInSeconds(Carbon::now());
+           $duration = Carbon::now()->timediffInSeconds($temp);
+          
           $duration=$duration/360;
         return $duration;
     }
