df2d5d811a8c1b64da55dff626a29abc974b3284
X <33039497+x@users.noreply.github.com>
2019-08-01 16:28:39 +0200

    larapage up2

--- project1/resources/views/Lara.blade.php
+++ project1/resources/views/Lara.blade.php
@@ -28,72 +28,107 @@
                         <th scope="row">{{$key+1}}</th>
                         <td><a href="{{$lara->link}}">{{$lara->name}}</a></td>
                         <td><a href="{{$lara->link}}">{{$lara->link}}</a></td>
-                        
-                        
-                       @if($lara->adress!='0')
-                       <td><a href="https://jira.godaddy.com/browse/{{$lara->adress}}">{{$lara->adress}}</a></td>
 
-                        @else
+                        @if($lara->adress == '0' && $lara->servername == '0' && $lara->location == '0' && lara->duration == '0')     
+
                         <td>
                             <div class="input-group mb-3">
                                 <input type="text" class="form-control" placeholder="DCSXB link" aria-label="DCSXB link" aria-describedby="button-addon1">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-                                </div>
                             </div>
                         </td>
-                        @endif
-                        
-                        @if($lara->servername!='0') <td>{{$lara->servername}}</td>
-
-                        @else
                         <td>
                             <div class="input-group mb-3">
                                 <input type="text" class="form-control" placeholder="Server name" aria-label="Server name" aria-describedby="button-addon1">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-                                </div>
                             </div>
                         </td>
-                        @endif
-
-
-                        @if($lara->location != '0') <td>{{$lara->location}}</td>
-
-                        @else
                         <td>
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="Server location" aria-label="Server location" aria-describedby="button-addon2">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-                                </div>
+                                <input type="text" class="form-control" placeholder="Server location" aria-label="Server location" aria-describedby="button-addon1">
                             </div>
                         </td>
-                        @endif
-
-                        @if($lara->duration!='0') <td>{{$lara->duration}}</td>
-                        @else
-                        <td>
-                            <div class="input-group mb-3">
-
-                                <select class="custom-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-                                    <option selected>Choose...</option>
-                                    <option value="1">30 min</option>
-                                    <option value="2">1h</option>
-                                    <option value="3">2h</option>
-                                    <option value="4">6h</option>
-                                    <option value="5">24h</option>
-                                </select>
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button">Button</button>
-                                </div>
-
-                            </div>
                         </td>
-                        @endif
+                <div class="input-group mb-3">
 
-                    </tr>
-                    @endforeach
+                    <select class="custom-select" id="inputGroupSelect04" aria-label="Selector">
+                        <option selected>Choose...</option>
+                        <option value="1">30 min</option>
+                        <option value="2">1h</option>
+                        <option value="3">2h</option>
+                        <option value="4">6h</option>
+                        <option value="5">24h</option>
+                    </select>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button">Button</button>
+                    </div>
+
+                </div>
+
+                </td>
+                @else
+                @if($lara->adress!='0')
+                <td><a href="https://jira.godaddy.com/browse/{{$lara->adress}}">{{$lara->adress}}</a></td>
+
+                @else
+                <td>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="DCSXB link" aria-label="DCSXB link" aria-describedby="button-addon1">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+                        </div>
+                    </div>
+                </td>
+                @endif
+
+                @if($lara->servername!='0') <td>{{$lara->servername}}</td>
+
+                @else
+                <td>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Server name" aria-label="Server name" aria-describedby="button-addon1">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+                        </div>
+                    </div>
+                </td>
+                @endif
+
+
+                @if($lara->location != '0') <td>{{$lara->location}}</td>
+
+                @else
+                <td>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Server location" aria-label="Server location" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                        </div>
+                    </div>
+                </td>
+                @endif
+
+                @if($lara->duration!='0') <td>{{$lara->duration}}</td>
+                @else
+                <td>
+                    <div class="input-group mb-3">
+
+                        <select class="custom-select" id="inputGroupSelect04" aria-label="Selector">
+                            <option selected>Choose...</option>
+                            <option value="1">30 min</option>
+                            <option value="2">1h</option>
+                            <option value="3">2h</option>
+                            <option value="4">6h</option>
+                            <option value="5">24h</option>
+                        </select>
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">Button</button>
+                        </div>
+
+                    </div>
+                </td>
+                @endif
+                @endif
+                </tr>
+                @endforeach
                 </tbody>
             </table>
 
