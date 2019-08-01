69dc992201c945f5003bb87f12e1a21e7732a43d
X <33039497+x@users.noreply.github.com>
2019-08-01 21:36:37 +0200

    bug6

--- project1/app/Lara.php
+++ project1/app/Lara.php
@@ -5,12 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
-class Lara extends Model
+class Lara extends Model 
 {
     //
       public function duration() {
-          $temp = $this->time;
-          $now =Carbon::now('Europe/Berlin');
+          $temp = Carbon::createFromDate($this->time);
+          $now = Carbon::now('Europe/Berlin');
           $duration = $temp->diffInMinutes($now);
         //   $duration = Carbon::now('Europe/Berlin')->diffInMinutes($temp);
            
