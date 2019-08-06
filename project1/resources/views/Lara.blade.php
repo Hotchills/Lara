c1151a22222c0534aaaa47f3e6aa02544afee997
X <33039497+x@users.noreply.github.com>
2019-08-06 12:21:37 +0200

    left

--- project1/resources/views/Lara.blade.php
+++ project1/resources/views/Lara.blade.php
@@ -116,8 +116,8 @@
                             </button>
                         </td>
                     </tr>
-                    <div class="collapse" id="collapse{{$lara->id}}" Style="position:absolute;width:50%;left:110%;top:20%;">
-                <div class="card card-body" Style="z-index:1000;width:50%;">
+                    <div class="collapse" id="collapse{{$lara->id}}" Style="position:absolute;width:50%;;top:20%;">
+                <div class="card card-body" Style="z-index:1000;left:50%;width:50%;">
                     <div class="input-group">
 
                         <textarea class="form-control" aria-label="Paste ticket info here" id="textarea{{$lara->id}}"></textarea>
